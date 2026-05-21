6461135b349c683a73fa1f0ba8046af5ad1e8a19
Koushik-UOS <ks100@stir.ac.uk>
2026-05-21 13:23:36 +0100

    Add files via upload

---
+++ lib/main.py
@@ -0,0 +1,41 @@
+import utime
+import machine
+# from utime import sleep_ms, sleep_us, ticks_ms, ticks_us, ticks_diff
+#import SCC
+import LoRaWANHandler
+from LoRaConfig import LoRaConfig
+
+def blink(count, delay):
+    for ind in range(count):
+        led.on()
+        utime.sleep_ms(delay // 2)
+        led.off()
+        utime.sleep_ms(delay // 2)
+
+print("This is the LoRa temperature and humidity measurement application.")
+LoRaWANHandler.getBoardID()
+
+LED_PIN = const(25)
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+
+lh = LoRaWANHandler.LoRaWANHandler(LoRaConfig)
+blink(3, 1000)
+lh.otaa()
+blink(3, 1000)
+#i2c = SCC.initSCC()
+utime.sleep_ms(5000)
+
+while(True):
+    #meas = SCC.measurementSCC(i2c)
+    lh = LoRaWANHandler.LoRaWANHandler(LoRaConfig)
+    blink(3, 1000)
+    lh.otaa()
+    blink(3, 1000)
+    print("Message Send phase")
+    meas = [0,1,2]
+    msg = "Koushik"
+    #print(msg)
+    lh.send(msg, False)
+    blink(2, 2000)
+    utime.sleep_ms(30000)
+
